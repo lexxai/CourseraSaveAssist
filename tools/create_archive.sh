8dfe9f247367b99aae9fc26b42d3f5b09a836861
lexxai <lexxai@users.noreply.github.com>
2024-01-18 04:51:38 +0200

    tune archive

--- tools/create_archive.sh
+++ tools/create_archive.sh
@@ -2,4 +2,5 @@
 cd ..
 ver=$(grep \"version\" manifest.json | sed -E 's/^.*: "([0-9.]*)",/\1/' | tr -dc '[0-9.]')
 filename="CourseraSaveAssist-${ver}"
+git pull origin main
 git archive  --worktree-attributes --format=zip --output ../${filename}.zip main
