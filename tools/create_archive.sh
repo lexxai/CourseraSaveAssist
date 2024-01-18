393e511f22103db4075d11c97ad93ddab6a77f12
lexxai <lexxai@users.noreply.github.com>
2024-01-18 04:45:32 +0200

    --worktree-attributes

--- tools/create_archive.sh
+++ tools/create_archive.sh
@@ -2,4 +2,4 @@
 cd ..
 ver=$(grep \"version\" manifest.json | sed -E 's/^.*: "([0-9.]*)",/\1/' | tr -dc '[0-9.]')
 filename="CourseraSaveAssist-${ver}"
-git archive --format=zip --output ../${filename}.zip main
+git archive  --worktree-attributes --format=zip --output ../${filename}.zip main
